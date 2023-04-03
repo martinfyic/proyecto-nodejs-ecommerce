cebf4c1ec17bf7944c26cccbc6142e5a88b91a8c
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-03 20:19:27 -0300

    feat: add fieldValidator.js middleware

---
+++ src/middlewares/fieldValidator.js
@@ -0,0 +1,9 @@
+import { validationResult } from 'express-validator';
+
+export const fieldValidator = (req, res, next) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(400).json(errors);
+	}
+	next();
+};
