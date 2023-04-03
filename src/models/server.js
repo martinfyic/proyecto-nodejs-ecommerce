68f81b7c40ad7de3b04ab70dc2f1f624095c8717
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-03 20:24:02 -0300

    feat: :sparkles: add server model

---
+++ src/models/server.js
@@ -0,0 +1,57 @@
+import express from 'express';
+import cors from 'cors';
+import { dbConnection } from '../database/config.js';
+import { authRouter } from '../routes/index.js';
+
+export class Server {
+	constructor() {
+		this.app = express();
+		this.PORT = process.env.PORT || 3000;
+
+		// Path
+		this.path = {
+			auth: '/api/auth',
+		};
+
+		// DB connect
+		this.connectDB();
+
+		// Middlewares
+		this.middlewares();
+
+		// Routes
+		this.routes();
+	}
+
+	async connectDB() {
+		await dbConnection();
+	}
+
+	middlewares() {
+		// cors
+		this.app.use(cors());
+		// Lectura y parseo
+		this.app.use(express.json());
+		this.app.use(express.urlencoded({ extended: true }));
+		// Dir publico
+		this.app.use(express.static('public'));
+	}
+
+	routes() {
+		this.app.use(this.path.auth, authRouter);
+	}
+
+	listen() {
+		this.app
+			.listen(this.PORT, () => {
+				console.log(
+					`===> 🚀 Servidor en http://localhost:${
+						this.PORT
+					} - ⌚ - ${new Date().toLocaleString()} 🚀`
+				);
+			})
+			.on('error', err => {
+				console.log(`===> ⚠️ Error en servidor: ${err?.message}`);
+			});
+	}
+}
