73517d7cae66625119aa8b2bcfbdc7bcadf76215
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-04 19:22:48 -0300

    feat: :sparkles: add src/routes/authRoutes.js

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+import { check } from 'express-validator';
+import { fieldValidator } from '../middlewares/index.js';
+import * as authController from '../controller/authController.js';
+
+export const authRouter = Router();
+
+authRouter.post(
+	'/login',
+	[
+		check('email', 'El email es obligatorio').isEmail(),
+		check('password', 'El password es obligatorio').not().isEmpty(),
+		fieldValidator,
+	],
+	authController.login
+);
+
+authRouter.post(
+	'/google',
+	[
+		check('id_token', 'El id_token es necesario').not().isEmpty(),
+		fieldValidator,
+	],
+	authController.googleSingIn
+);
