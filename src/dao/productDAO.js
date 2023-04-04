7557e3f80d649fc3486ccefcaea6650dbe0e5026
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-04 19:10:43 -0300

    feat: :construction: add product DAO

---
+++ src/dao/productDAO.js
@@ -0,0 +1,25 @@
+import { Product } from '../models/index.js';
+
+export const getAllProducts = async (limit, since) => {
+	const activProduct = { state: true };
+
+	const [products, totalProducts] = await Promise.all([
+		Product.find(activProduct)
+			.populate('user', 'name')
+			.populate('category', 'name')
+			.limit(Number(limit))
+			.skip(Number(since)),
+		Product.countDocuments(activProduct),
+	]);
+
+	return [products, totalProducts];
+};
+
+export const getProductById = async id => {
+	const product = await Product.findById(id)
+		.populate('user', ['name', 'state'])
+		.populate('category', 'name')
+		.exec();
+
+	return product;
+};
