0120c4450ef7974eaaacd98b27ca7c2e610b39a2
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-04 19:15:38 -0300

    feat: :zap: add googleVerify.js helper

---
+++ src/helpers/googleVerify.js
@@ -0,0 +1,19 @@
+import { OAuth2Client } from 'google-auth-library';
+
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
+async function googleVerify(token = '') {
+	const ticket = await client.verifyIdToken({
+		idToken: token,
+		audience: process.env.GOOGLE_CLIENT_ID,
+	});
+	const { name, picture, email } = ticket.getPayload();
+
+	return {
+		name,
+		img: picture,
+		email,
+	};
+}
+
+export default googleVerify;
