2344f4b11f17583bdb4055bef04052c5c993156d
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-04 23:08:34 -0300

    feat: :sparkles: create product add postProduct in productController

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -1,4 +1,5 @@
 import { productService } from '../service/index.js';
+import { productDTO } from '../dto/index.js';
 
 export const getProducts = async (req, res) => {
 	const { limit = 5, since = 0 } = req.query;
@@ -25,6 +26,30 @@ export const getProductById = async (req = request, res = response) => {
 	return res.status(200).json({
 		status: 'Ok',
 		message: 'Producto encontrado',
+		method: req.method,
+		product,
+	});
+};
+
+export const postProduct = async (req, res) => {
+	const { state, user, ...body } = req.body;
+	const name = body.name.toUpperCase();
+
+	const existProduct = await productService.getProductByName(name);
+
+	if (existProduct) {
+		return res.status(400).json({
+			message: `El producto ${existProduct.name} ya existe`,
+		});
+	}
+
+	const newProduct = productDTO(name, req.user._id, ...body);
+
+	const product = await productService.postProduct(newProduct);
+
+	return res.status(201).json({
+		status: 'Ok',
+		message: 'Producto creado correctamente',
 		method: req.method,
 		product,
 	});
