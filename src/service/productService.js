c09376809815e687956538b50fd77fcc14247cb4
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-04 23:20:08 -0300

    feat: :sparkles: add postProduct in productService

--- src/service/productService.js
+++ src/service/productService.js
@@ -1,11 +1,45 @@
 import { productDAO } from '../dao/index.js';
 
 export const getAllProducts = async (limit, since) => {
-	const allProducts = await productDAO.getAllProducts(limit, since);
-	return allProducts;
+	try {
+		const allProducts = await productDAO.getAllProducts(limit, since);
+		return allProducts;
+	} catch (error) {
+		console.log(
+			`===> ⚠️ Error en getAllProducts-productService - ⌚ - ${new Date().toLocaleString()} ==> ${error}`
+		);
+	}
 };
 
 export const getProductById = async id => {
-	const product = await productDAO.getProductById(id);
-	return product;
+	try {
+		const product = await productDAO.getProductById(id);
+		return product;
+	} catch (error) {
+		console.log(
+			`===> ⚠️ Error en getProductById-productService - ⌚ - ${new Date().toLocaleString()} ==> ${error}`
+		);
+	}
+};
+
+export const getProductByName = async name => {
+	try {
+		const product = await productDAO.getProductByName(name);
+		return product;
+	} catch (error) {
+		console.log(
+			`===> ⚠️ Error en getProductById-productService - ⌚ - ${new Date().toLocaleString()} ==> ${error}`
+		);
+	}
+};
+
+export const postProduct = async product => {
+	try {
+		const newProduct = await productDAO.postProduct(product);
+		return newProduct;
+	} catch (error) {
+		console.log(
+			`===> ⚠️ Error en postProduct-productService - ⌚ - ${new Date().toLocaleString()} ==> ${error}`
+		);
+	}
 };
