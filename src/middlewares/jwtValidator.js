980c9078110a0be93f6c2355596c3fc114e77ec3
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-04 23:17:26 -0300

    feat: :sparkles: add src/middlewares/jwtValidator.js

---
+++ src/middlewares/jwtValidator.js
@@ -0,0 +1,38 @@
+import jwt from 'jsonwebtoken';
+import { User } from '../models/index.js';
+
+export const jwtValidator = async (req, res, next) => {
+	const token = req.header('auth-token');
+
+	if (!token) {
+		return res.status(401).json({
+			message: 'No hay token en la peticion',
+		});
+	}
+
+	try {
+		const { uid } = jwt.verify(token, process.env.SECRET_PRIVATE_KEY);
+
+		const user = await User.findById(uid).exec();
+
+		if (!user) {
+			return res.status(401).json({
+				message: 'Token no valido',
+			});
+		}
+
+		if (!user.state) {
+			return res.status(401).json({
+				message: 'Token no valido',
+			});
+		}
+
+		req.user = user;
+		next();
+	} catch (error) {
+		console.log(`⚠️ ==> ${error}`);
+		return res.status(401).json({
+			message: 'Token no valido',
+		});
+	}
+};
