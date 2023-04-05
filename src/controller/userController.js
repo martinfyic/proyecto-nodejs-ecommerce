21893099586085bb0b02ca9c39468f251f15a756
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-04 23:09:06 -0300

    feat: :sparkles: add src/controller/userController.js

---
+++ src/controller/userController.js
@@ -0,0 +1,56 @@
+import { userService } from '../service/index.js';
+
+export const getUsers = async (req, res) => {
+	const { limit = 10, since = 0 } = req.query;
+
+	const [users, totalUsers] = await userService.getUsers(limit, since);
+
+	res.json({
+		message: 'Usuarios en DB',
+		totalUsers,
+		users,
+	});
+};
+
+export const getUserById = async (req, res) => {
+	const { id } = req.params;
+	const user = await userService.getUserById(id);
+
+	res.json({
+		message: `Usuario ID: ${id} encontrado`,
+		user,
+	});
+};
+
+export const postUser = async (req, res) => {
+	const body = req.body;
+	const user = await userService.postUser(body);
+
+	res.json({
+		message: 'Usuario creado correctamente',
+		user,
+	});
+};
+
+export const putUser = async (req, res) => {
+	const { id } = req.params;
+	const body = req.body;
+
+	const user = await userService.putUser(id, body);
+
+	res.json({
+		message: 'Usuario actualizado correctamente',
+		user,
+	});
+};
+
+export const deleteUser = async (req, res) => {
+	const { id } = req.params;
+
+	const user = await userService.deleteUser(id);
+
+	return res.json({
+		message: 'Usuario desactivado de la DB',
+		user,
+	});
+};
