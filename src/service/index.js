fde475c60752a41e1bf62c83d15d8542a8055ac6
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-05 21:34:28 -0300

    chore: :building_construction: add categoryService in src/service/index.js for barrel export

--- src/service/index.js
+++ src/service/index.js
@@ -1,3 +1,4 @@
 export * as authService from './authService.js';
+export * as categoryService from './categoryService.js';
 export * as productService from './productService.js';
 export * as userService from './userService.js';
