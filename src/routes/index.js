ea9139e4c308548744683e8c881847f164011c13
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-05 21:33:48 -0300

    chore: :building_construction: add categoryRouter in src/routes/index.js for barrel export

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,4 @@
 export { authRouter } from './authRoutes.js';
+export { categoryRouter } from './categoryRoutes.js';
 export { productRouter } from './productRoutes.js';
 export { userRouter } from './userRouter.js';
