78a3a8cadc38a0e494e014ceeeb7014236d1d0a8
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-06 20:13:11 -0300

    perf: :lock: use bcrypt to encode password login with google

--- src/dto/authDTO.js
+++ src/dto/authDTO.js
@@ -1,8 +1,15 @@
+import { v4 as uuidv4 } from 'uuid';
+import bcrypt from 'bcryptjs';
+
+const uuidPass = uuidv4();
+const salt = bcrypt.genSaltSync();
+const passwordToSave = bcrypt.hashSync(uuidPass, salt);
+
 export const newUserDTO = user => {
 	const newUser = {
 		name: user.name,
 		email: user.email,
-		password: 'google',
+		password: passwordToSave,
 		role: 'USER_ROLE',
 		picture: user.picture,
 		google: true,
