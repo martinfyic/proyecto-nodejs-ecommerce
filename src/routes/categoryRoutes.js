28324e5cb77fe5e9470efa9bcd310745b6915c40
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-06 22:58:32 -0300

    feat: :sparkles: created put & delete endpoint in src/routes/categoryRoutes.js

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -1,20 +1,59 @@
 import { Router } from 'express';
 import { check } from 'express-validator';
-import { fieldValidator, jwtValidator } from '../middlewares/index.js';
+import {
+	fieldValidator,
+	jwtValidator,
+	isAdminRole,
+} from '../middlewares/index.js';
+import { categoryByIdExist } from '../helpers/index.js';
 import * as categoryController from '../controller/categoryController.js';
 
 export const categoryRouter = Router();
 
 categoryRouter.get('/', categoryController.getCategories);
 
-categoryRouter.get('/:id', [], categoryController.getCategoryById);
+categoryRouter.get(
+	'/:id',
+	[
+		check('id', 'El ID no es valido').isMongoId(),
+		check('id').custom(categoryByIdExist),
+		fieldValidator,
+	],
+	categoryController.getCategoryById
+);
 
 categoryRouter.post(
 	'/',
 	[
 		jwtValidator,
+		isAdminRole,
 		check('name', 'El nombre es obligatorio').not().isEmpty(),
 		fieldValidator,
 	],
 	categoryController.postCategory
+);
+
+categoryRouter.put(
+	'/:id',
+	[
+		jwtValidator,
+		isAdminRole,
+		check('name', 'El nombre es obligatorio').not().isEmpty(),
+		check('id', 'El ID no es valido').isMongoId(),
+		check('id').custom(categoryByIdExist),
+		fieldValidator,
+	],
+	categoryController.updateCategory
+);
+
+categoryRouter.delete(
+	'/:id',
+	[
+		jwtValidator,
+		isAdminRole,
+		check('id', 'El ID no es valido').isMongoId(),
+		check('id').custom(categoryByIdExist),
+		fieldValidator,
+	],
+	categoryController.deletedCategory
 );
