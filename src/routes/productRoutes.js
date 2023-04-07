7430c3e48f91de1dc3aeb8cdd3e0e95d49ed6cb4
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-06 22:07:36 -0300

    feat: :sparkles: added put & delete endpoint in productRoutes

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -2,7 +2,11 @@ import { Router } from 'express';
 import { check } from 'express-validator';
 import * as productController from '../controller/productController.js';
 import { productByIdExist, categoryByIdExist } from '../helpers/index.js';
-import { fieldValidator, jwtValidator } from '../middlewares/index.js';
+import {
+	fieldValidator,
+	jwtValidator,
+	isAdminRole,
+} from '../middlewares/index.js';
 
 export const productRouter = Router();
 
@@ -22,10 +26,43 @@ productRouter.post(
 	'/',
 	[
 		jwtValidator,
+		isAdminRole,
 		check('name', 'El nombre es obligatorio').not().isEmpty(),
 		check('category', 'No es un ID de mongo').isMongoId(),
 		check('category').custom(categoryByIdExist),
+		check('precio', 'El precio es obligatorio y numerico')
+			.not()
+			.isEmpty()
+			.isNumeric(),
+		check('stock', 'El stock es obligatorio y numerico')
+			.not()
+			.isEmpty()
+			.isNumeric(),
 		fieldValidator,
 	],
 	productController.postProduct
+);
+
+productRouter.put(
+	'/:id',
+	[
+		jwtValidator,
+		isAdminRole,
+		check('id', 'El ID no es valido').isMongoId(),
+		check('id').custom(productByIdExist),
+		fieldValidator,
+	],
+	productController.updateProduct
+);
+
+productRouter.delete(
+	'/:id',
+	[
+		jwtValidator,
+		isAdminRole,
+		check('id', 'El ID no es valido').isMongoId(),
+		check('id').custom(productByIdExist),
+		fieldValidator,
+	],
+	productController.deleteProduct
 );
