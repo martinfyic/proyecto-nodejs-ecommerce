d57da1bedb6c334c47d7b18d7954be38f813208e
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-07 22:24:07 -0300

    chore: :building_construction: include searchRouter into src/routes/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 export { authRouter } from './authRoutes.js';
 export { categoryRouter } from './categoryRoutes.js';
 export { productRouter } from './productRoutes.js';
+export { searchRouter } from './searchRoutes.js';
 export { userRouter } from './userRouter.js';
