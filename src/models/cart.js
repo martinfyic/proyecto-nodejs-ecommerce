e662b573c228de209a7b676936dfa0ba442442c1
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-08 23:24:24 -0300

    feat: :sparkles: add cart schema src/models/cart.js

---
+++ src/models/cart.js
@@ -0,0 +1,34 @@
+import { Schema, model } from 'mongoose';
+
+const CartSchema = Schema(
+	{
+		products: {
+			type: Array,
+			ref: 'Products',
+			required: true,
+			default: [],
+		},
+
+		user: {
+			type: Schema.Types.ObjectId,
+			ref: 'Users',
+			required: true,
+		},
+
+		state: {
+			type: Boolean,
+			default: true,
+			required: true,
+			description: 'Estado del carrito de compras (activo/inactivo)',
+		},
+	},
+	{ timestamps: true, versionKey: false }
+);
+
+CartSchema.methods.toJSON = function () {
+	const { state, _id, ...cart } = this.toObject();
+	cart.cartId = _id;
+	return cart;
+};
+
+export const Cart = model('Carts', CartSchema);
