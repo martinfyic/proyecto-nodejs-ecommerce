e88ece4c3fac5172ae50112ffe8df91209271ba3
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-08 23:26:23 -0300

    perf: :zap: include timestamps: true, versionKey: false into src/model/user.js

--- src/models/user.js
+++ src/models/user.js
@@ -1,38 +1,43 @@
 import { Schema, model } from 'mongoose';
 
-const UserSchema = Schema({
-	name: {
-		type: String,
-		required: [true, 'El nombre es obligatorio'],
+const UserSchema = Schema(
+	{
+		name: {
+			type: String,
+			required: [true, 'El nombre es obligatorio'],
+		},
+		email: {
+			type: String,
+			required: [true, 'El mail es obligatorio'],
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: [true, 'La contraseña es obligatorio'],
+		},
+		img: {
+			type: String,
+		},
+		role: {
+			type: String,
+			required: true,
+		},
+		state: {
+			type: Boolean,
+			default: true,
+			description: 'Estado del usuario (activo/inactivo)',
+		},
+		google: {
+			type: Boolean,
+			default: false,
+			description: 'loggin con google (verdadero/falso)',
+		},
 	},
-	email: {
-		type: String,
-		required: [true, 'El mail es obligatorio'],
-		unique: true,
-	},
-	password: {
-		type: String,
-		required: [true, 'La contraseña es obligatorio'],
-	},
-	img: {
-		type: String,
-	},
-	role: {
-		type: String,
-		required: true,
-	},
-	state: {
-		type: Boolean,
-		default: true,
-	},
-	google: {
-		type: Boolean,
-		default: false,
-	},
-});
+	{ timestamps: true, versionKey: false }
+);
 
 UserSchema.methods.toJSON = function () {
-	const { __v, password, _id, ...user } = this.toObject();
+	const { password, _id, ...user } = this.toObject();
 	user.uid = _id;
 	return user;
 };
