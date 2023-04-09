04b19288028485bdfb58becfd61b047ff13f317b
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-08 23:19:15 -0300

    chore: :building_construction: add cartService.js into src/service/index.js to barrel export

--- src/service/index.js
+++ src/service/index.js
@@ -1,4 +1,5 @@
 export * as authService from './authService.js';
+export * as cartService from './cartService.js';
 export * as categoryService from './categoryService.js';
 export * as productService from './productService.js';
 export * as searchService from './searchService.js';
