f93431fcd52b585ae24f6196dc8f6c868dabac13
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-08 23:21:39 -0300

    chore: :building_construction: include cartDAO into src/dao/index.js for barrel export

--- src/dao/index.js
+++ src/dao/index.js
@@ -1,4 +1,5 @@
 export * as authDAO from './authDAO.js';
+export * as cartDAO from './cartDAO.js';
 export * as categoryDAO from './categoryDAO.js';
 export * as productDAO from './productDAO.js';
 export * as searchDAO from './searchDAO.js';
