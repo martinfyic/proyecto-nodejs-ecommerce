8dc92811c8f22d2a5b9691865bf803863f11679d
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-08 23:30:23 -0300

    perf: :zap: include timestamps: true, versionKey: false

--- src/models/category.js
+++ src/models/category.js
@@ -1,25 +1,29 @@
 import { Schema, model } from 'mongoose';
 
-const CategorySchema = Schema({
-	name: {
-		type: String,
-		required: [true, 'El nombre es obligatorio'],
-		unique: true,
+const CategorySchema = Schema(
+	{
+		name: {
+			type: String,
+			required: [true, 'El nombre es obligatorio'],
+			unique: true,
+		},
+		state: {
+			type: Boolean,
+			default: true,
+			required: true,
+			description: 'Estado de la categoria (activo/inactivo)',
+		},
+		user: {
+			type: Schema.Types.ObjectId,
+			ref: 'Users',
+			required: true,
+		},
 	},
-	state: {
-		type: Boolean,
-		default: true,
-		required: true,
-	},
-	user: {
-		type: Schema.Types.ObjectId,
-		ref: 'Users',
-		required: true,
-	},
-});
+	{ timestamps: true, versionKey: false }
+);
 
 CategorySchema.methods.toJSON = function () {
-	const { __v, state, ...category } = this.toObject();
+	const { state, ...category } = this.toObject();
 	if (category.user._id) {
 		category.user.uid = category.user._id;
 		delete category.user._id;
