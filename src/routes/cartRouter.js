c7cb2169d739bff8a32267de675534add5269b56
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-08 23:33:15 -0300

    feat: :construction: add src/routes/cartRouter.js

---
+++ src/routes/cartRouter.js
@@ -0,0 +1,45 @@
+import { Router } from 'express';
+import { check } from 'express-validator';
+import * as cartController from '../controller/cartController.js';
+import { fieldValidator, jwtValidator } from '../middlewares/index.js';
+import { cartByIdExist, productByIdExist } from '../helpers/index.js';
+
+export const cartRouter = Router();
+
+cartRouter.get('/', cartController.getAllCarts);
+
+cartRouter.get(
+	'/:id',
+	[
+		check('id', 'El ID no es valido').isMongoId(),
+		check('id').custom(cartByIdExist),
+		fieldValidator,
+	],
+	cartController.getCart
+);
+
+cartRouter.post('/', [jwtValidator, fieldValidator], cartController.postCart);
+
+cartRouter.delete(
+	'/:id',
+	[
+		jwtValidator,
+		check('id', 'El ID no es valido').isMongoId(),
+		check('id').custom(cartByIdExist),
+		fieldValidator,
+	],
+	cartController.deleteCart
+);
+
+cartRouter.post(
+	'/:id/products',
+	[
+		jwtValidator,
+		check('id', 'El cartID no es valido').isMongoId(),
+		check('id').custom(cartByIdExist),
+		check('prodId', 'El prodID no es valido').isMongoId(),
+		check('prodId').custom(productByIdExist),
+		fieldValidator,
+	],
+	cartController.addProductToCart
+);
