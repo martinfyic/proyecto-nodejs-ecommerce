34f9a63336b1a91fc65856f4a713906687859633
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-08 23:34:51 -0300

    feat: :construction: add src/services/cartService.js

---
+++ src/service/cartService.js
@@ -0,0 +1,35 @@
+import { cartDAO, productDAO } from '../dao/index.js';
+import { cartDTO } from '../dto/index.js';
+
+export const getAllCarts = async (limit, since) => {
+	const allCarts = await cartDAO.getAllCarts(limit, since);
+	return allCarts;
+};
+
+export const getCart = async id => {
+	const cart = await cartDAO.getCart(id);
+	return cart;
+};
+
+export const findCartByUser = async userId => {
+	const findCartUser = await cartDAO.findCartByUser(userId);
+	return findCartUser;
+};
+
+export const postCart = async userId => {
+	const cartFormat = cartDTO(userId);
+
+	const cartGenerated = await cartDAO.postCart(cartFormat);
+	return cartGenerated;
+};
+
+export const deleteCart = async id => {
+	const cartDeleted = await cartDAO.deleteCart(id);
+	return cartDeleted;
+};
+
+export const addProductToCart = async (id, prodId) => {
+	const productAdded = await productDAO.getProductById(prodId);
+	const cart = await cartDAO.addProductToCart(id, productAdded);
+	return cart;
+};
