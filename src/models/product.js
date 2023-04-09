46cccef7d0bad212046412138846125730acf419
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-08 23:32:23 -0300

    perf: :zap: included timestamps: true, versionKey: false

--- src/models/product.js
+++ src/models/product.js
@@ -1,52 +1,56 @@
 import { Schema, model } from 'mongoose';
 
-const ProductSchema = Schema({
-	name: {
-		type: String,
-		required: [true, 'El nombre es obligatorio'],
-		unique: true,
+const ProductSchema = Schema(
+	{
+		name: {
+			type: String,
+			required: [true, 'El nombre es obligatorio'],
+			unique: true,
+		},
+
+		state: {
+			type: Boolean,
+			default: true,
+			required: true,
+			description: 'Estado del producto (activo/inactivo)',
+		},
+
+		user: {
+			type: Schema.Types.ObjectId,
+			ref: 'Users',
+			required: true,
+		},
+
+		price: {
+			type: Number,
+			required: [true, 'El precio es obligatorio'],
+		},
+
+		category: {
+			type: Schema.Types.ObjectId,
+			ref: 'Categories',
+			required: [true, 'La categoria es obligatoria'],
+		},
+
+		description: { type: String },
+
+		available: {
+			type: Boolean,
+			default: true,
+		},
+
+		img: { type: String },
+
+		stock: {
+			type: Number,
+			required: true,
+		},
 	},
-
-	state: {
-		type: Boolean,
-		default: true,
-		required: true,
-	},
-
-	user: {
-		type: Schema.Types.ObjectId,
-		ref: 'Users',
-		required: true,
-	},
-
-	precio: {
-		type: Number,
-		required: [true, 'El precio es obligatorio'],
-	},
-
-	category: {
-		type: Schema.Types.ObjectId,
-		ref: 'Categories',
-		required: [true, 'La categoria es obligatoria'],
-	},
-
-	description: { type: String },
-
-	available: {
-		type: Boolean,
-		default: true,
-	},
-
-	img: { type: String },
-
-	stock: {
-		type: Number,
-		required: true,
-	},
-});
+	{ timestamps: true, versionKey: false }
+);
 
 ProductSchema.methods.toJSON = function () {
-	const { __v, state, ...product } = this.toObject();
+	const { state, ...product } = this.toObject();
 	if (product.user._id) {
 		product.user.uid = product.user._id;
 		delete product.user._id;
