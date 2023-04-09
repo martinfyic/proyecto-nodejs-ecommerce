e55889ce9cc26f9c088e9c3d6163c29f07fc8299
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-08 23:20:53 -0300

    perf: :zap: add timestamps: true, versionKey: false

--- src/models/role.js
+++ src/models/role.js
@@ -1,10 +1,13 @@
 import { Schema, model } from 'mongoose';
 
-const RoleSchema = Schema({
-	role: {
-		type: String,
-		required: [true, 'El rol es obligatorio'],
+const RoleSchema = Schema(
+	{
+		role: {
+			type: String,
+			required: [true, 'El rol es obligatorio'],
+		},
 	},
-});
+	{ timestamps: true, versionKey: false }
+);
 
 export const Role = model('Roles', RoleSchema);
