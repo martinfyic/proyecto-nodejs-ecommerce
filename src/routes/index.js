8af10f7c4acda4a27fbb330ee601e392e606e277
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-08 23:33:49 -0300

    chore: :building_construction: include cartRouter into src/routes/index.js for barrel export

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 export { authRouter } from './authRoutes.js';
+export { cartRouter } from './cartRouter.js';
 export { categoryRouter } from './categoryRoutes.js';
 export { productRouter } from './productRoutes.js';
 export { searchRouter } from './searchRoutes.js';
