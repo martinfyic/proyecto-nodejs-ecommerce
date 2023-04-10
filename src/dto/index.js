94e1acde7f6d46e6ae6e98e3c3534e74e92dbe3d
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-09 22:54:48 -0300

    chore: :building_construction: inluce orderDTO.js in src/dto/index.js for export barrel

--- src/dto/index.js
+++ src/dto/index.js
@@ -1,5 +1,6 @@
 export * from './authDTO.js';
 export * from './cartDTO.js';
 export * from './categoryDTO.js';
+export * from './orderDTO.js';
 export * from './productDTO.js';
 export * from './userDTO.js';
