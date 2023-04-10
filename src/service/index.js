57a4160b1f3f076ffa443c793e9fd1b9cac9c1cd
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-09 22:58:43 -0300

    chore: :building_construction: include orderServices into src/service/index.js for export barrel

--- src/service/index.js
+++ src/service/index.js
@@ -1,6 +1,7 @@
 export * as authService from './authService.js';
 export * as cartService from './cartService.js';
 export * as categoryService from './categoryService.js';
+export * as orderService from './orderService.js';
 export * as productService from './productService.js';
 export * as searchService from './searchService.js';
 export * as userService from './userService.js';
