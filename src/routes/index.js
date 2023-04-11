763766e6be40f282d66d51fd14d8ac643d0d54bc
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-11 20:48:46 -0300

    chore: :building_construction: include uploadsRouter into src/routes/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,4 +4,5 @@ export { categoryRouter } from './categoryRoutes.js';
 export { orderRouter } from './orderRoutes.js';
 export { productRouter } from './productRoutes.js';
 export { searchRouter } from './searchRoutes.js';
+export { uploadsRouter } from './uploadsRoutes.js';
 export { userRouter } from './userRouter.js';
