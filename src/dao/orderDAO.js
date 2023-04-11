a6827b81465d4f350461431f57c8f59b1311d66b
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-10 22:42:52 -0300

    perf: :loud_sound: add error logs in orderDAO

--- src/dao/orderDAO.js
+++ src/dao/orderDAO.js
@@ -1,23 +1,31 @@
 import { Order } from '../models/index.js';
 
 export const getAllOrders = async (limit, since) => {
-	const activOrder = { state: true };
+	try {
+		const activOrder = { state: true };
 
-	const [orders, totalOrders] = await Promise.all([
-		Order.find(activOrder)
-			.populate('purchase', ['products', 'user'])
-			.limit(Number(limit))
-			.skip(Number(since))
-			.lean()
-			.exec(),
-		Order.countDocuments(activOrder),
-	]);
+		const [orders, totalOrders] = await Promise.all([
+			Order.find(activOrder)
+				.populate('purchase', ['products', 'user'])
+				.limit(Number(limit))
+				.skip(Number(since))
+				.lean()
+				.exec(),
+			Order.countDocuments(activOrder),
+		]);
 
-	return [orders, totalOrders];
+		return [orders, totalOrders];
+	} catch (error) {
+		logger.error(`===> ⚠️ Error in orderDAO/getAllOrders: ${error}`);
+	}
 };
 
 export const createOrder = async orderFormat => {
-	const newOrder = new Order(orderFormat);
-	await newOrder.save();
-	return newOrder;
+	try {
+		const newOrder = new Order(orderFormat);
+		await newOrder.save();
+		return newOrder;
+	} catch (error) {
+		logger.error(`===> ⚠️ Error in orderDAO/createOrder: ${error}`);
+	}
 };
