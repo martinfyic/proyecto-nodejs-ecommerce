ba8f548e1357b1fba27e4c98e8245dc1594338ca
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-11 20:45:00 -0300

    chore: :building_construction: include archiveValidator into src/middleware/index.js

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,3 +1,4 @@
+export { archiveValidator } from './archiveValidator.js';
 export { fieldValidator } from './fieldValidator.js';
 export { jwtValidator } from './jwtValidator.js';
 export { isAdminRole } from './roleValidator.js';
