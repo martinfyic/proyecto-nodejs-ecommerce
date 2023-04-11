445e627bb4882fbe2424934ce5c662825d048e9e
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-11 20:38:52 -0300

    perf: :zap: include address in orderDTO

--- src/dto/orderDTO.js
+++ src/dto/orderDTO.js
@@ -2,12 +2,13 @@ import { logger } from '../config/winston/winston.js';
 
 export const orderDTO = (cartOrder, user) => {
 	try {
-		const { _id, name, email } = user;
+		const { _id, name, email, address } = user;
 		const dataFormat = {
 			purchase: cartOrder.products,
 			userId: _id,
 			userEmail: email,
 			userName: name,
+			userAddress: address,
 		};
 
 		return dataFormat;
