1c366d08b7c823ea618c4b2922462a8b06db8a44
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-10 22:41:59 -0300

    perf: :loud_sound: add error logs in orderController

--- src/controller/orderController.js
+++ src/controller/orderController.js
@@ -1,36 +1,45 @@
 import { orderService } from '../service/index.js';
+import { logger } from '../config/winston/winston.js';
 
 export const getAllOrders = async (req, res) => {
-	const { limit = 10, since = 0 } = req.query;
-	const [orders, totalOrders] = await orderService.getAllOrders(limit, since);
+	try {
+		const { limit = 10, since = 0 } = req.query;
+		const [orders, totalOrders] = await orderService.getAllOrders(limit, since);
 
-	return res.json({
-		status: 'Ok',
-		message: 'Ordenes Activas',
-		method: req.method,
-		totalOrders,
-		orders,
-	});
+		return res.json({
+			status: 'Ok',
+			message: 'Ordenes Activas',
+			method: req.method,
+			totalOrders,
+			orders,
+		});
+	} catch (error) {
+		logger.error(`===> ⚠️ Error in orderController/getAllOrders: ${error}`);
+	}
 };
 
 export const createOrder = async (req, res) => {
-	const { idCart } = req.params;
+	try {
+		const { idCart } = req.params;
 
-	const newOrder = await orderService.createOrder(idCart);
+		const newOrder = await orderService.createOrder(idCart, req.user);
 
-	if (newOrder.message) {
-		return res.status(400).json({
-			status: 'Bad request',
-			message: newOrder.message,
+		if (newOrder.message) {
+			return res.status(400).json({
+				status: 'Bad request',
+				message: newOrder.message,
+				method: req.method,
+				cart: newOrder.cart,
+			});
+		}
+
+		return res.status(201).json({
+			status: 'Ok',
+			message: `Orden ${newOrder._id} creada exitosamente`,
 			method: req.method,
-			cart: newOrder.cart,
+			newOrder,
 		});
+	} catch (error) {
+		logger.error(`===> ⚠️ Error in orderController/createOrder: ${error}`);
 	}
-
-	return res.status(201).json({
-		status: 'Ok',
-		message: `Orden ${newOrder._id} creada exitosamente`,
-		method: req.method,
-		newOrder,
-	});
 };
