41f56993634619e6c294bf981191f95cfb78c7dd
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-11 21:18:03 -0300

    perf: :zap: try catch wrapped searchRouter

--- src/routes/searchRoutes.js
+++ src/routes/searchRoutes.js
@@ -1,11 +1,18 @@
 import { Router } from 'express';
 import { fieldValidator, jwtValidator } from '../middlewares/index.js';
 import searchController from '../controller/searchController.js';
+import { logger } from '../config/winston/winston.js';
 
 export const searchRouter = Router();
 
-searchRouter.get(
-	'/:collection/:term',
-	[jwtValidator, fieldValidator],
-	searchController
-);
+try {
+	searchRouter.get(
+		'/:collection/:term',
+		[jwtValidator, fieldValidator],
+		searchController
+	);
+} catch (error) {
+	logger.error(
+		`===> ⚠️ Error in searchRoutes/searchRouter.get '/:collection/:term': ${error}`
+	);
+}
