c99cb998df31a1f524aac6b590ca8e47d40bebd7
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-12 21:23:17 -0300

    feat: :sparkles: include findOutJWT into src/helpers/createJWT.js

--- src/helpers/createJWT.js
+++ src/helpers/createJWT.js
@@ -1,24 +1,44 @@
 import jwt from 'jsonwebtoken';
+import { User } from '../models/index.js';
 import { logger } from '../config/winston/winston.js';
 
 export const createJWT = (uid = '') => {
-	return new Promise((resolve, reject) => {
-		const payload = { uid };
+	try {
+		return new Promise((resolve, reject) => {
+			const payload = { uid };
 
-		jwt.sign(
-			payload,
-			process.env.SECRET_PRIVATE_KEY,
-			{
-				expiresIn: '2h',
-			},
-			(error, token) => {
-				if (error) {
-					logger.error(`⚠️ ==> ${error}`);
-					reject('No se pudo generar el token');
-				} else {
-					resolve(token);
+			jwt.sign(
+				payload,
+				process.env.SECRET_PRIVATE_KEY,
+				{
+					expiresIn: '2h',
+				},
+				(error, token) => {
+					if (error) {
+						logger.error(`⚠️ ==> ${error}`);
+						reject('No se pudo generar el token');
+					} else {
+						resolve(token);
+					}
 				}
-			}
-		);
-	});
+			);
+		});
+	} catch (error) {
+		logger.error(`===> ⚠️ Error en helpers/createJWT: ${error}`);
+	}
+};
+
+export const findOutJWT = async (token = '') => {
+	try {
+		if (token.length <= 10) return null;
+		const { uid } = jwt.verify(token, process.env.SECRET_PRIVATE_KEY);
+		const user = await User.findById(uid).exec();
+
+		if (!user) return null;
+		if (!user.state) return null;
+
+		return user;
+	} catch (error) {
+		logger.error(`===> ⚠️ Error en helpers/findOutJWT: ${error}`);
+	}
 };
