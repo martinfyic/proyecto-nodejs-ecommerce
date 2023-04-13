b24cb7d2069233da93952182670e9c218818aeb5
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-12 21:20:25 -0300

    fix: :bug: change the name of Server to ServerApp

--- src/models/index.js
+++ src/models/index.js
@@ -3,5 +3,5 @@ export { Category } from './category.js';
 export { Order } from './order.js';
 export { Product } from './product.js';
 export { Role } from './role.js';
-export { Server } from './server.js';
+export { ServerApp } from './server.js';
 export { User } from './user.js';
