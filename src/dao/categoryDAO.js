818b3f4eb29f151d7ecefdba95654c1fa459a864
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-15 18:28:22 -0300

    perf: :zap: include try catch in functions categoryDAO

--- src/dao/categoryDAO.js
+++ src/dao/categoryDAO.js
@@ -1,46 +1,71 @@
 import { Category } from '../models/index.js';
+import { logger } from '../config/winston/winston.js';
 
 export const getCategories = async (limit, since) => {
-	const activCategory = { state: true };
-	const [categories, totalCategories] = await Promise.all([
-		Category.find(activCategory)
-			.populate('user', 'name')
-			.limit(Number(limit))
-			.skip(Number(since)),
-		Category.countDocuments(activCategory),
-	]);
+	try {
+		const activCategory = { state: true };
+		const [categories, totalCategories] = await Promise.all([
+			Category.find(activCategory)
+				.populate('user', 'name')
+				.limit(Number(limit))
+				.skip(Number(since)),
+			Category.countDocuments(activCategory),
+		]);
 
-	return [categories, totalCategories];
+		return [categories, totalCategories];
+	} catch (error) {
+		logger.error(`===> ⚠️ Error in categoryDAO/getCategories: ${error}`);
+	}
 };
 
 export const getCategoryById = async id => {
-	const category = await Category.findById(id).lean().exec();
-	return category;
+	try {
+		const category = await Category.findById(id).lean().exec();
+		return category;
+	} catch (error) {
+		logger.error(`===> ⚠️ Error in categoryDAO/getCategoryById: ${error}`);
+	}
 };
 
 export const getCategoryByName = async name => {
-	const categoryName = await Category.findOne({ name }).lean().exec();
-	return categoryName;
+	try {
+		const categoryName = await Category.findOne({ name }).lean().exec();
+		return categoryName;
+	} catch (error) {
+		logger.error(`===> ⚠️ Error in categoryDAO/getCategoryByName: ${error}`);
+	}
 };
 
 export const postCategory = async name => {
-	const newCategory = new Category(name);
-	await newCategory.save();
-	return newCategory;
+	try {
+		const newCategory = new Category(name);
+		await newCategory.save();
+		return newCategory;
+	} catch (error) {
+		logger.error(`===> ⚠️ Error in categoryDAO/postCategory: ${error}`);
+	}
 };
 
 export const updateCategory = async (id, body) => {
-	const categoryUpdated = await Category.findByIdAndUpdate(id, body, {
-		new: true,
-	});
-	return categoryUpdated;
+	try {
+		const categoryUpdated = await Category.findByIdAndUpdate(id, body, {
+			new: true,
+		});
+		return categoryUpdated;
+	} catch (error) {
+		logger.error(`===> ⚠️ Error in categoryDAO/updateCategory: ${error}`);
+	}
 };
 
 export const deletedCategory = async id => {
-	const categoryDeleted = await Category.findByIdAndUpdate(
-		id,
-		{ state: false },
-		{ new: true }
-	);
-	return categoryDeleted;
+	try {
+		const categoryDeleted = await Category.findByIdAndUpdate(
+			id,
+			{ state: false },
+			{ new: true }
+		);
+		return categoryDeleted;
+	} catch (error) {
+		logger.error(`===> ⚠️ Error in categoryDAO/deletedCategory: ${error}`);
+	}
 };
