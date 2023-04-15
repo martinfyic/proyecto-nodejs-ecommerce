62efbb079fdbd7340dbd462f96b0e03d8691ebec
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-15 18:33:09 -0300

    perf: :zap: include try catch in functions cartService

--- src/service/cartService.js
+++ src/service/cartService.js
@@ -1,40 +1,69 @@
 import { cartDAO, productDAO } from '../dao/index.js';
 import { cartDTO } from '../dto/index.js';
+import { logger } from '../config/winston/winston.js';
 
 export const getAllCarts = async (limit, since) => {
-	const allCarts = await cartDAO.getAllCarts(limit, since);
-	return allCarts;
+	try {
+		const allCarts = await cartDAO.getAllCarts(limit, since);
+		return allCarts;
+	} catch (error) {
+		logger.error(`===> ⚠️ Error in cartService/getAllCarts: ${error}`);
+	}
 };
 
 export const getCart = async id => {
-	const cart = await cartDAO.getCart(id);
-	return cart;
+	try {
+		const cart = await cartDAO.getCart(id);
+		return cart;
+	} catch (error) {
+		logger.error(`===> ⚠️ Error in cartService/getCart: ${error}`);
+	}
 };
 
 export const findCartByUser = async userId => {
-	const findCartUser = await cartDAO.findCartByUser(userId);
-	return findCartUser;
+	try {
+		const findCartUser = await cartDAO.findCartByUser(userId);
+		return findCartUser;
+	} catch (error) {
+		logger.error(`===> ⚠️ Error in cartService/findCartByUser: ${error}`);
+	}
 };
 
 export const postCart = async userId => {
-	const cartFormat = cartDTO(userId);
+	try {
+		const cartFormat = cartDTO(userId);
 
-	const cartGenerated = await cartDAO.postCart(cartFormat);
-	return cartGenerated;
+		const cartGenerated = await cartDAO.postCart(cartFormat);
+		return cartGenerated;
+	} catch (error) {
+		logger.error(`===> ⚠️ Error in cartService/postCart: ${error}`);
+	}
 };
 
 export const deleteCart = async id => {
-	const cartDeleted = await cartDAO.deleteCart(id);
-	return cartDeleted;
+	try {
+		const cartDeleted = await cartDAO.deleteCart(id);
+		return cartDeleted;
+	} catch (error) {
+		logger.error(`===> ⚠️ Error in cartService/deleteCart: ${error}`);
+	}
 };
 
 export const addProductToCart = async (id, prodId) => {
-	const productAdded = await productDAO.getProductById(prodId);
-	const cart = await cartDAO.addProductToCart(id, productAdded);
-	return cart;
+	try {
+		const productAdded = await productDAO.getProductById(prodId);
+		const cart = await cartDAO.addProductToCart(id, productAdded);
+		return cart;
+	} catch (error) {
+		logger.error(`===> ⚠️ Error in cartService/addProductToCart: ${error}`);
+	}
 };
 
 export const deleteProductInCart = async (id, prodId) => {
-	const cart = await cartDAO.deleteProductInCart(id, prodId);
-	return cart;
+	try {
+		const cart = await cartDAO.deleteProductInCart(id, prodId);
+		return cart;
+	} catch (error) {
+		logger.error(`===> ⚠️ Error in cartService/deleteProductInCart: ${error}`);
+	}
 };
