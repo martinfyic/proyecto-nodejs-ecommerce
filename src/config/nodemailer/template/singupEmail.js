0d5c27a98a2387ba3a78d455361540d8fd40e958
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-15 18:25:11 -0300

    perf: :zap: include try catch in functions

--- src/config/nodemailer/template/singupEmail.js
+++ src/config/nodemailer/template/singupEmail.js
@@ -1,61 +1,70 @@
 import { transporter } from '../nodemailer.js';
+import { logger } from '../../winston/winston.js';
 
 export const singupEmailAdmin = async body => {
-	const email = await transporter.sendMail({
-		from: `"Nodemailer ⚡" <${process.env.NODEMAILER_EMAIL}>`,
-		to: `${process.env.NODEMAILER_EMAIL_ADMIN}`,
-		subject: 'Nuevo registro en DB ✔',
-		html: `
-			<h1>Nuevo registro en DB 🔥</h1>
-			<hr/>
-			<h3>Informacion del Usuario:</h3>
-			<ul>
-				<li>
-					<p><strong>Nombre:</strong> ${body.name}</p>
-				</li>
-				<li>
-					<strong>Role:</strong> ${body.role}</p>
-				</li>
-				<li>
-					<strong>Email:</strong> ${body.email}</p>
-				</li>
-				<li>
-					<strong>Logueado con google:</strong> ${body.google}</p>
-				</li>
-				<li>
-					<strong>Alta de usuario:</strong> ${body.createdAt}</p>
-				</li>
-			</ul>
-			<hr/>
-			`,
-	});
+	try {
+		const email = await transporter.sendMail({
+			from: `"Nodemailer ⚡" <${process.env.NODEMAILER_EMAIL}>`,
+			to: `${process.env.NODEMAILER_EMAIL_ADMIN}`,
+			subject: 'Nuevo registro en DB ✔',
+			html: `
+				<h1>Nuevo registro en DB 🔥</h1>
+				<hr/>
+				<h3>Informacion del Usuario:</h3>
+				<ul>
+					<li>
+						<p><strong>Nombre:</strong> ${body.name}</p>
+					</li>
+					<li>
+						<strong>Role:</strong> ${body.role}</p>
+					</li>
+					<li>
+						<strong>Email:</strong> ${body.email}</p>
+					</li>
+					<li>
+						<strong>Logueado con google:</strong> ${body.google}</p>
+					</li>
+					<li>
+						<strong>Alta de usuario:</strong> ${body.createdAt}</p>
+					</li>
+				</ul>
+				<hr/>
+				`,
+		});
 
-	return email;
+		return email;
+	} catch (error) {
+		logger.error(`===> ⚠️ Nodemailer - singupEmailAdmin error: ${error}`);
+	}
 };
 
 export const singupEmailUser = async body => {
-	const email = await transporter.sendMail({
-		from: `"Nodemailer ⚡" <${process.env.NODEMAILER_EMAIL}>`,
-		to: `${body.email}`,
-		subject: 'Registro exitoso ✔',
-		html: `
-			<h1>Gracias por registrarte 🚀</h1>
-			<hr/>
-			<h3>Informacion del Usuario:</h3>
-			<ul>
-				<li>
-					<p><strong>Nombre:</strong> ${body.name}</p>
-				</li>
-				<li>
-					<strong>Email:</strong> ${body.email}</p>
-				</li>
-				<li>
-					<strong>Alta de usuario:</strong> ${body.createdAt}</p>
-				</li>
-			</ul>
-			<hr/>
-			`,
-	});
+	try {
+		const email = await transporter.sendMail({
+			from: `"Nodemailer ⚡" <${process.env.NODEMAILER_EMAIL}>`,
+			to: `${body.email}`,
+			subject: 'Registro exitoso ✔',
+			html: `
+				<h1>Gracias por registrarte 🚀</h1>
+				<hr/>
+				<h3>Informacion del Usuario:</h3>
+				<ul>
+					<li>
+						<p><strong>Nombre:</strong> ${body.name}</p>
+					</li>
+					<li>
+						<strong>Email:</strong> ${body.email}</p>
+					</li>
+					<li>
+						<strong>Alta de usuario:</strong> ${body.createdAt}</p>
+					</li>
+				</ul>
+				<hr/>
+				`,
+		});
 
-	return email;
+		return email;
+	} catch (error) {
+		logger.error(`===> ⚠️ Nodemailer - singupEmailUser error: ${error}`);
+	}
 };
